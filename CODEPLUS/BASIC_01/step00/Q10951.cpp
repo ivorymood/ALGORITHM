f65f827f3213ff3af42cc0dcef270131c00f7267
LHY <ivorymood@gmail.com>
2020-04-13 15:34:29 +0900

    A+B - 4

---
+++ CODEPLUS/BASIC_01/step00/Q10951.cpp
@@ -0,0 +1,15 @@
+//https://www.acmicpc.net/submit/10951
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A, B;
+	while (cin >> A >> B)
+	{
+		cout << A + B << '\n';
+	}
+
+	return 0;
+}
