ca68f4a8004667cb5c01de6f332aa19002b9d0f1
LHY <ivorymood@gmail.com>
2020-04-13 15:50:07 +0900

    A+B - 7

---
+++ CODEPLUS/BASIC_01/step00/Q11021.cpp
@@ -0,0 +1,18 @@
+//https://www.acmicpc.net/problem/11021
+#pragma warning(disable : 4996)
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	
+	for (int i = 0; i < n; i++)
+	{
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", i + 1, a + b);
+	}
+
+	return 0;
+}
