372a6cf0a0c98092caa90caca94d1fba5cb152db
LHY <ivorymood@gmail.com>
2020-04-13 15:45:36 +0900

    A+B - 6

---
+++ CODEPLUS/BASIC_01/step00/Q10953.cpp
@@ -0,0 +1,20 @@
+//https://www.acmicpc.net/problem/10953
+#pragma warning(disable: 4996)
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	while (N-- > 0)
+	{
+		int A, B;
+		scanf("%d,%d", &A, &B);
+		printf("%d\n", A + B);
+	}
+
+	return 0;
+}
