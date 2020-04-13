76fa0fd58f24e08320b2f010a92f708b9536e75d
LHY <ivorymood@gmail.com>
2020-04-13 16:50:47 +0900

    열 개씩 끊어 출력하기

---
+++ CODEPLUS/BASIC_01/step00/Q11721.cpp
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/11721
+#pragma warning(disable : 4996)
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	char input[101];
+	while (scanf("%10s", input) != EOF)
+	{
+		printf("%s\n", input);
+	}
+
+	return 0;
+}
