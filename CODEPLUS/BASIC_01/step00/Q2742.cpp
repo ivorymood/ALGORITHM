1a6be2b7965bc12dcdc7a799572d636138878b75
LHY <ivorymood@gmail.com>
2020-04-13 17:27:26 +0900

    기찍 N

---
+++ CODEPLUS/BASIC_01/step00/Q2742.cpp
@@ -0,0 +1,17 @@
+//https://www.acmicpc.net/problem/2742
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	while(n > 0)
+	{
+		printf("%d\n", n);
+		n--;
+	}
+
+	return 0;
+}
