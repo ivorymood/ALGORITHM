f9d80059ec89ea3cc3d6dce0654dd48b68ac3ac5
LHY <ivorymood@gmail.com>
2020-04-13 17:26:02 +0900

    N 찍기

---
+++ CODEPLUS/BASIC_01/step00/Q2741.cpp
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/2741
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
