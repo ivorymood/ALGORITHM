3e10c938361922cf8fc447bf45912afb3d004f0a
LHY <ivorymood@gmail.com>
2020-04-18 14:12:43 +0900

    나머지

---
+++ CODEPLUS/BASIC_01/step01/Q10430.cpp
@@ -0,0 +1,21 @@
+//https://www.acmicpc.net/problem/10430
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int A, B, C;
+	cin >> A >> B >> C;
+	
+	cout << (A + B) % C << '\n';
+	cout << ((A % C) + (B % C)) % C << '\n';
+	cout << (A * B) % C << '\n';
+	cout << ((A % C) * (B % C)) % C << '\n';
+
+	return 0;
+}
