7bfb43edc3b10b5d1e5c7f6054a5e66942b483f8
LHY <ivorymood@gmail.com>
2020-04-19 18:16:52 +0900

    1, 2, 3 더하기 - top-down

---
+++ CODEPLUS/BASIC_01/step01/Q9095_ver1.cpp
@@ -0,0 +1,43 @@
+//https://www.acmicpc.net/problem/9095
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 10;
+int D[MAX + 1] = {1, };
+
+// Á¡È­½Ä : D[n] = D[n - 1] + D[n - 2] + D[n - 3]
+int get_123_sum(int n)
+{
+	if (D[n] > 0)
+	{
+		return D[n];
+	}
+	if (n <= 2)
+	{
+		return D[n] = n;
+	}
+
+	return D[n] = get_123_sum(n - 1) + get_123_sum(n - 2)+ get_123_sum(n - 3);
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int cnt;
+	cin >> cnt;
+	while (cnt--)
+	{
+		int n;
+		cin >> n;
+
+		get_123_sum(n);
+
+		cout << D[n] << '\n';
+	}
+
+	return 0;
+}
