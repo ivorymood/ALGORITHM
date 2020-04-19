c682b48deb7082dca0c1abf2bbdefb4928e306d1
LHY <ivorymood@gmail.com>
2020-04-19 19:54:00 +0900

    카드 구매하기 - bottom-up

---
+++ CODEPLUS/BASIC_01/step01/Q11052_ver2.cpp
@@ -0,0 +1,36 @@
+//https://www.acmicpc.net/problem/11052
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<int> cost(n + 1);
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> cost[i];
+	}
+
+	// Á¡È­½Ä : D[n] = max(D[n - i] + P[i])
+	vector<int> max_cost(n + 1);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			max_cost[i] = max(max_cost[i], max_cost[i - j] + cost[j]);
+		}
+	}
+
+	cout << max_cost[n];
+
+	return 0;
+}
