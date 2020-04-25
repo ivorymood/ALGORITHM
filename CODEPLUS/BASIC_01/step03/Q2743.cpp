4e750cbd8fb93bb529bb9fb5be229f56a2a7c400
LHY <ivorymood@gmail.com>
2020-04-25 15:14:11 +0900

    단어 길이 재기

---
+++ CODEPLUS/BASIC_01/step03/Q2743.cpp
@@ -0,0 +1,17 @@
+//https://www.acmicpc.net/problem/2743
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string input;
+	cin >> input;
+	cout << input.size();
+
+	return 0;
+}
