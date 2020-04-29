c9d921f980a0e921783e1869bf8b2e5925d178d4
LHY <ivorymood@gmail.com>
2020-04-29 19:41:19 +0900

    쇠막대기

---
+++ PROGRMMERS/LEVEL_02/Q42585.cpp
@@ -0,0 +1,47 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42585
+//쇠막대기
+#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string arrangement) {
+    int answer = 0;
+    int stick = 0;
+
+    for (int i = 0; i < arrangement.size(); i++)
+    {
+        // 막대기 시작
+        if (arrangement[i] == '(')
+        {
+            stick++;
+        }
+        else
+        {
+            stick--;
+
+            // 레이저 일때
+            if (arrangement[i - 1] == '(')
+            {
+                answer += stick;
+            }
+            // 그냥 막대기 끝 일때
+            else
+            {
+                answer++;
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    string arrangement = "()(((()())(())()))(())";
+    cout << solution(arrangement);
+
+    return 0;
+}
