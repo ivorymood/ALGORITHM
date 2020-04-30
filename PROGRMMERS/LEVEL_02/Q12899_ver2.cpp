3d7030568d7b2d856cb4c5fa5d231a596e2a411a
LHY <ivorymood@gmail.com>
2020-04-30 16:02:50 +0900

    124 나라의 숫자 - 바로 변환

---
+++ PROGRMMERS/LEVEL_02/Q12899_ver2.cpp
@@ -0,0 +1,41 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12899
+//124 나라의 숫자
+#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+
+    string answer = "";
+    // 3 진법 012 = 124나라의 412
+    char num[] = { '4', '1', '2' };
+
+    while (n > 0)
+    {
+        int rest = n % 3;
+        n /= 3;
+
+        // 124나라에는 0이 없기 때문에 나머지가 0일 경우, n -= 1
+        if (rest == 0)
+        {
+            n--;
+        }
+
+        answer = num[rest] + answer;
+    }
+
+    return answer;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    cout << solution(n);
+
+    return 0;
+}
