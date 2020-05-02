65a12d7ae025ac0c253bfe0f9e211b29f3d519e7
LHY <ivorymood@gmail.com>
2020-05-02 18:25:59 +0900

    사탕게임 - 라이브러리 활용

---
+++ CODEPLUS/BASIC_02/step01/Q3085_ver2.cpp
@@ -0,0 +1,135 @@
+//https://www.acmicpc.net/problem/3085
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int get_max_after_swap(const vector<string> &board, int x, int y)
+{
+	int N = board.size();
+	int max = 0;
+
+	// 가로
+	for (int i = x; i < N && i < x + 2; i++)
+	{
+		int h_max = 1;
+		for (int j = 0; j < N - 1; j++)
+		{
+			if (board[i][j] == board[i][j + 1])
+			{
+				h_max++;
+				max = (max < h_max) ? h_max : max;
+			}
+			else
+			{
+				h_max = 1;
+			}
+		}
+	}
+
+	// 세로
+	for (int j = y; j < N && j < y + 2; j++)
+	{
+		int v_max = 1;
+		for (int i = 0; i < N - 1; i++)
+		{
+			if (board[i][j] == board[i + 1][j])
+			{
+				v_max++;
+				max = (max < v_max) ? v_max : max;
+			}
+			else
+			{
+				v_max = 1;
+			}
+		}
+	}
+	return max;
+}
+
+int get_max_count(const vector<string> &board)
+{
+	int N = board.size();
+	int max = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		// 가로
+		int t_max = 1;
+		for (int j = 0; j < N - 1; j++)
+		{
+			if (board[i][j] == board[i][j + 1])
+			{
+				t_max++;
+				max = (max < t_max) ? t_max : max;
+			}
+			else
+			{
+				t_max = 1;
+			}
+		}
+
+		// 세로
+		t_max = 1;
+		for (int j = 0; j < N - 1; j++)
+		{
+			if (board[j][i] == board[j + 1][i])
+			{
+				t_max++;
+				max = (max < t_max) ? t_max : max;
+			}
+			else
+			{
+				t_max = 1;
+			}
+		}
+	}
+	return max;
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector<string> board(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> board[i];
+	}
+
+	// 인접한 사탕을 교환하기 전, 전체 보드에서 사탕 최대개수 구하기
+	int max = get_max_count(board);
+
+	// 인접한 두 사탕을 가로/세로로 교환하면서 사탕 최대개수 구하기
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			// 가로
+			if (j < N - 1)
+			{
+				swap(board[i][j], board[i][j + 1]);
+				// 교환한 사탕이 포함된 2*2 행/렬만 검사
+				int t_max = get_max_after_swap(board, i, j);
+				swap(board[i][j], board[i][j + 1]);
+				max = (max < t_max) ? t_max : max;
+			}
+
+			// 세로
+			if (i < N - 1)
+			{
+				swap(board[i][j], board[i + 1][j]);
+				// 교환한 사탕이 포함된 2*2 행/렬만 검사
+				int t_max = get_max_after_swap(board, i, j);
+				swap(board[i][j], board[i + 1][j]);
+				max = (max < t_max) ? t_max : max;
+			}
+		}
+	}
+
+	cout << max;
+
+	return 0;
+}
