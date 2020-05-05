bb2212f98ae1353b71145e6b0815f9e03e2b8b97
LHY <ivorymood@gmail.com>
2020-05-05 11:34:04 +0900

    N과 M (4)

--- CODEPLUS/BASIC_02/step01/Q15652.cpp
+++ CODEPLUS/BASIC_02/step01/Q15652.cpp
@@ -8,12 +8,14 @@ char output[MAX * 2 + 1];
 
 void perm(int N, int M, int depth, int start)
 {
+	// depth가 M에 도달하면 출력
 	if (depth == M)
 	{
 		cout << output << '\n';
 		return;
 	}
 
+	// 중복 허용, 비내림차순(다음 숫자가 현재 숫자보다 같거나 큰 순서)인 수열 입력
 	for (int i = start; i <= N; i++)
 	{
 		output[depth * 2] = i + '0';
@@ -28,9 +30,11 @@ int main()
 	cin.tie(0);
 	cout.tie(0);
 
+	// 입력
 	int N, M;
 	cin >> N >> M;
 
+	// 수열 출력
 	perm(N, M, 0, 1);
 
 	return 0;
