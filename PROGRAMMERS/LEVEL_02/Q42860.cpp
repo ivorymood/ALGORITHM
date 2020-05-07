92ade8a2e810cdc9c14ca3484ff6fc803d984ddc
LHY <ivorymood@gmail.com>
2020-05-07 21:57:03 +0900

    조이스틱

--- PROGRAMMERS/LEVEL_02/Q42860.cpp
+++ PROGRAMMERS/LEVEL_02/Q42860.cpp
@@ -15,6 +15,7 @@ int solution(string name) {
     // 알파벳 바꾸는 횟수
     for (int i = 0; i < len; ++i)
     {
+        // A가 아닌 문자면 상하 이동
         if (name[i] != 'A')
         {
             answer += min(name[i] - 'A', 'Z' + 1 - name[i]);
@@ -25,15 +26,20 @@ int solution(string name) {
     int left_min = len - 1;
     for (int i = 0; i < len; ++i)
     {
+        // A가 아닌 문자면 좌우 이동
         if (name[i] != 'A')
         {
+            // 다음 A가 아닌 인덱스 구하기
             int next_idx = i + 1;
             while (next_idx < len && name[next_idx] == 'A')
             {
                 ++next_idx;
             }
 
+            // i 위치에서의 총 왼쪽 이동 횟수 구하기
             int left_move = i * 2 + len - next_idx;
+
+            // 최소 이동횟수
             left_min = min(left_min, left_move);
         }
     }
