6427c31aef5fb823017b7b944453eaec1eb4333d
LHY <ivorymood@gmail.com>
2020-05-19 14:05:01 +0900

    단체사진 찍기

--- PROGRAMMERS/LEVEL_02/Q1835.cpp
+++ PROGRAMMERS/LEVEL_02/Q1835.cpp
@@ -4,51 +4,53 @@
 
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-bool check(int order[], int n, vector<string> &data)
+bool check(int order[], int n, vector<string> data)
 {
-    for (int i = 0 ; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         string d = data[i];
+        // 두 문자 위치 사이의 간격
         int distance = abs(order[d[0] - 'A'] - order[d[2] - 'A']) - 1;
 
-        printf("%c ~ %c : %d \n", d[0], d[2], distance);
-
-        if (!(d[3] == '=' && distance == d[4])
-            && !(d[3] == '>' && distance > d[4])
-            && !(d[3] == '<' && distance < d[4]))
+        // 문제 조건에 일치하지 않으면 false
+        if (!(d[3] == '=' && distance == d[4] - '0')
+            && !(d[3] == '>' && distance > d[4] - '0')
+            && !(d[3] == '<' && distance < d[4] - '0'))
         {
             return false;
         }
     }
 
+    // 문제 조건이 모두 일치하면 true
     return true;
 }
 
-// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int n, vector<string> data) {
-    
+
     char perm[8] = { 'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T' };
     int cnt = 0;
 
+    // 오름차순 순열 -> 위치값 저장
     do {
         int order[26] = { 0, };
-        for (int i = 1; i <= 8; ++i)
+        for (int i = 0; i < 8; ++i)
         {
-            order[perm[i - 1] - 'A'] = i;
+            order[perm[i] - 'A'] = i + 1;
         }
 
+        // 해당 순열이 조건에 맞으면 cnt++
         if (check(order, n, data))
         {
             ++cnt;
         }
-    
+
     } while (next_permutation(perm, perm + 8));
-    
+
     return cnt;
 }
 
