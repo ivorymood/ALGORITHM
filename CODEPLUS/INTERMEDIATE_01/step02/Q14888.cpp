992e6d97214c9733eda20f6fb1acd18a65f7affa
LHY <ivorymood@gmail.com>
2020-06-02 03:00:14 +0900

    연산자 끼워넣기 - 백트랙킹

---
+++ CODEPLUS/INTERMEDIATE_01/step02/Q14888.cpp
@@ -0,0 +1,86 @@
+//https://www.acmicpc.net/problem/14888
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+using namespace std;
+
+// 답안의 범위가 -10억 ~ 10억
+int max_num = numeric_limits<int>::min();
+int min_num = numeric_limits<int>::max();
+
+// 주어진 연산자 인덱스로 계산
+int calc(int op, int num1, int num2)
+{
+	if (op == 0)
+	{
+		return num1 + num2;
+	}
+	else if (op == 1)
+	{
+		return num1 - num2;
+	}
+	else if (op == 2)
+	{
+		return num1 * num2;
+	}
+	else if (op == 3)
+	{
+		return num1 / num2;
+	}
+	return 0;
+}
+
+void dfs(vector<int> &nums, vector<int> &strg, int res, int depth)
+{
+	if (depth == nums.size())
+	{
+		max_num = max(max_num, res);
+		min_num = min(min_num, res);
+		return;
+	}
+
+	for (int j = 0; j < 4; ++j)
+	{
+		// 연산 가능한 횟수가 남아있으면
+		if (strg[j] > 0)
+		{
+			--strg[j];
+			// 이전 결과와 현재숫자를 연산한 값으로 dfs함수 재귀호출
+			dfs(nums, strg, calc(j, res, nums[depth]), depth + 1);
+			++strg[j];
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	// 입력
+	int N;
+	cin >> N;
+	vector<int> nums(N);
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> nums[i];
+	}
+
+	vector<int> strg(4);
+	for (int i = 0; i < 4; ++i)
+	{
+		cin >> strg[i];
+	}
+
+	// 백트랙킹으로 가능한 모든 수를 구해서 최대값/최소값 결정하기
+	dfs(nums, strg, nums[0], 1);
+
+	// 출력
+	cout << max_num << '\n';
+	cout << min_num << '\n';
+
+	return 0;
+}
