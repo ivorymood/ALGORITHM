3e5ab1157ac80c591fcf92f5c45afc69fe8a2658
LHY <ivorymood@gmail.com>
2020-06-16 22:12:07 +0900

    아기상어 - bfs + 재귀

--- CODEPLUS/INTERMEDIATE_01/step02/Q16236_ver2.cpp
+++ CODEPLUS/INTERMEDIATE_01/step02/Q16236_ver2.cpp
@@ -16,7 +16,7 @@ int dist[MAX][MAX];
 int dr_x[] = { -1, 0, 1, 0 };
 int dr_y[] = { 0, -1, 0, 1 };
 
-int bfs(pos start, int size)
+pos bfs_get_next_pos(pos start, int size)
 {
 	memset(dist, -1, sizeof(dist));
 
@@ -24,17 +24,22 @@ int bfs(pos start, int size)
 	q.push(start);
 	dist[start.x][start.y] = 0;
 
+	pos next = { N, N };
+	bool fish = false;
+	int min_dist = 987654321;	// 먹을 수 있는 물고기와의 최소거리
+
 	while (!q.empty())
 	{
 		pos p = q.front();
 		q.pop();
 
-		// 상어가 먹을 수 있는 물고기일 때 거리값 반환
-		if (map[p.x][p.y] > 0 && map[p.x][p.y] < size)
+		// min_dist보다 큰 경로는 의미가 없다
+		if (dist[p.x][p.y] > min_dist)
 		{
-			return dist[p.x][p.y];
+			continue;
 		}
 
+		// 4방향 탐색
 		for (int i = 0; i < 4; ++i)
 		{
 			int nx = p.x + dr_x[i];
@@ -45,50 +50,52 @@ int bfs(pos start, int size)
 				continue;
 			}
 
-			// 이미 거리값을 매겼거나, 물고기가 상어보다 크면
+			// 이미 거리를 구했거나 물고기사이즈가 더 클 때
 			if (dist[nx][ny] >= 0 || map[nx][ny] > size)
 			{
 				continue;
 			}
 
-			// dist배열에 거리값을 저장하고 큐에 push
+			// 거리값을 저장 한 후 큐에 push
 			dist[nx][ny] = dist[p.x][p.y] + 1;
 			q.push({ nx, ny });
-		}
-	}
-	return -1;
-}
 
-pos get_next_pos(int size, int closest)
-{
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = 0; j < N; ++j)
-		{
-			if (dist[i][j] == closest && map[i][j] > 0 && map[i][j] < size)
+			// 먹을 수 있는 물고기이면
+			if (map[nx][ny] > 0 && map[nx][ny] < size)
 			{
-				return { i, j };
+				// 거리가 min_dst보다 크면 X
+				if (dist[nx][ny] > min_dist)
+				{
+					continue;
+				}
+				// 맨 위, 맨 왼쪽의 먹을 수 있는 물고기 위치 구하기
+				if ((nx < next.x || (nx == next.x && ny < next.y)))
+				{
+					next = { nx, ny };
+					min_dist = dist[nx][ny];
+				}
+
+				fish = true;
 			}
 		}
 	}
-	return { -1, -1 };
+
+	// 먹을 수 있는 물고기가 없다면
+	if (!fish)
+	{
+		next = { -1, -1 };
+	}
+	return next;
 }
 
 int fish_time(pos start, int size, int ate, int time)
 {
-	// 먹을수 있는 가장 가까운 물고기와의 거리
-	int closest_ok_fish = bfs(start, size);
-	if (closest_ok_fish < 0)
+	// 먹을 수 있는 다음 물고기 위치를 bfs로 탐색
+	pos next = bfs_get_next_pos(start, size);
+	if (next.x == -1)
 	{
 		return time;
 	}
-
-	/*
-		위에서 구한 closest_ok_fish와 동일한 거리에 있고
-		먹을 수 있는 맨위, 맨 왼쪽의 물고기 위치
-	*/
-	pos next = get_next_pos(size, closest_ok_fish);
-	map[next.x][next.y] = 0;
 
 	// 현재 size만큼 먹으면 상어는 크기가 1 커진다
 	if (++ate == size)
@@ -101,6 +108,7 @@ int fish_time(pos start, int size, int ate, int time)
 		상어의 위치는 위에서 구한 물고기 위치로 대체.
 		시간은 물고기 위치까지의 거리를 더한 값으로 fish_time 재귀호출
 	*/
+	map[next.x][next.y] = 0;
 	return fish_time(next, size, ate, time + dist[next.x][next.y]);
 }
 
@@ -110,7 +118,6 @@ int main()
 	cin.tie(0);
 	cout.tie(0);
 
-	// 입력
 	cin >> N;
 	pos start;
 	for (int i = 0; i < N; ++i)
@@ -119,7 +126,7 @@ int main()
 		{
 			cin >> map[i][j];
 
-			// 아기상어 일때
+			// 아기 상어 위치 저장 후 map[i][j]은 0으로 대체
 			if (map[i][j] == 9)
 			{
 				start = { i, j };
