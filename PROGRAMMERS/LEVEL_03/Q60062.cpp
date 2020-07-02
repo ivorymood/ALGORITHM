e62fcb57bc1c9ef9b3a16db0499049f026f23af0
LHY <ivorymood@gmail.com>
2020-07-03 00:54:28 +0900

    외벽 점검 - 브루트포스

--- PROGRAMMERS/LEVEL_03/Q60062.cpp
+++ PROGRAMMERS/LEVEL_03/Q60062.cpp
@@ -17,22 +17,15 @@ int solution(int n, vector<int> weak, vector<int> dist) {
     int d_size = dist.size();
     int answer = -1;
 
-    // 취약지점 파견을 시작하는 모든 경우의 수 구하기
+    // 원형탐색을 위해 weak벡터 뒤쪽에 각 요소보다 n씩 큰 요소 추가
     for (int i = 0; i < w_size; ++i)
     {
-        /*
-            한칸씩 앞으로 이동하고, 맨 앞칸은 맨 뒤로 보내기
+        weak.push_back(weak[i] + n);
+    }
 
-            ex) 1 5 6 10  --> 5 6 10 13 
-            마지막 13 = 1 + 12(n)
-        */
-        int tmp = weak[0] + n;
-        for (int j = 1; j < w_size; ++j)
-        {
-            weak[j - 1] = weak[j];
-        }
-        weak[w_size - 1] = tmp;
-
+    // 원형탐색: w_idx부터 탐색 시작
+    for (int w_idx = 0; w_idx < w_size; ++w_idx)
+    {
         /*
             ex) n = 50, weak = [1, 5, 10, 16, 22, 25], dist = [3, 4, 6] 의 경우
                 4:(1, 5),  6:(10, 16),  3:(22, 25) 만이 가능하다.
@@ -41,24 +34,25 @@ int solution(int n, vector<int> weak, vector<int> dist) {
         */
         do {
             
-            int w_idx = 0;
+            int passed = 0;
             int d_idx = 0;
 
-            while (d_idx < d_size && w_idx < w_size)
+            // weak과 dist의 인덱스가 범위 하에 있으면
+            while (d_idx < d_size && passed < w_size)
             {
                 // 친구가 최대로 닿을 수 있는 지점
-                int end = weak[w_idx] + dist[d_idx];
+                int end = weak[w_idx + passed] + dist[d_idx];
 
-                // 그 안에 있는 취약지점은 방문 완료 처리
-                while (w_idx < w_size && weak[w_idx] <= end)
+                // 그 사이에 있는 취약지점은 방문 완료 처리
+                while (passed < w_size && weak[w_idx + passed] <= end)
                 {
-                    ++w_idx;
+                    ++passed;
                 }
                 ++d_idx;
             }
 
-            // 모든 취약지점을 다 방문했을 때
-            if (w_idx == w_size)
+            // 모든 취약지점을 다 방문했을 때 : 가장 적은 파견 친구수 갱신
+            if (passed == w_size)
             {
                 answer = (answer < 0) ? d_idx : min(d_idx, answer);
             }
