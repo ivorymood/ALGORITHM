28666d1e636eb571e808bd7f18a6c8bd7832089a
hylim <ivorymood@gmail.com>
2021-07-13 10:55:19 +0900

    구명보트

---
+++ PROGRAMMERS/LEVEL_02/Q42885.java
@@ -0,0 +1,38 @@
+package PROGRAMMERS.LEVEL_02;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Q42885 {
+
+    public static void main(String[] args) {
+        /*
+        people	            limit	return
+        [70, 50, 80, 50]	100	    3
+        [70, 80, 50]	    100	    3
+         */
+        int[] people = {70, 50, 80, 50};
+        int limit = 100;
+        System.out.println(solution(people, limit));
+    }
+
+    public static int solution(int[] people, int limit) {
+
+        Arrays.sort(people);
+        int boat = 0;
+        int low = 0;
+        int high = people.length - 1;
+
+        while (low <= high) {
+
+            if (people[high] + people[low] <= limit) {
+                ++low;
+            }
+            --high;
+            ++boat;
+        }
+
+        return boat;
+    }
+}
